f4a6fa56373a815137d9322f50c1486d9726aed8
Zhydani <zhydani@gmail.com>
2023-07-16 17:13:45 -0300

    improviment!: adicionando o card com a imagem e sobre o cafe

--- src/pages/Home/components/CoffeeList/index.tsx
+++ src/pages/Home/components/CoffeeList/index.tsx
@@ -1,4 +1,5 @@
-import { TitleBox } from './styles'
+import { Card, CoffeeListBox, TitleBox } from './styles'
+import coffee from '../../../../assets/coffees/expresso.png'
 
 export function CoffeeList() {
   return (
@@ -6,6 +7,20 @@ export function CoffeeList() {
       <TitleBox>
         <h1>Nossos cafés</h1>
       </TitleBox>
+      <CoffeeListBox>
+        <Card>
+          <img src={coffee} alt="" />
+          <span>Tradicional</span>
+          <h1>Expresso Tradicional</h1>
+          <p>O tradicional café feito com água quente e grãos moídos</p>
+          <footer>
+            <div>
+              <span>R$</span>
+              <span>9,90</span>
+            </div>
+          </footer>
+        </Card>
+      </CoffeeListBox>
     </div>
   )
 }
--- src/pages/Home/components/CoffeeList/styles.ts
+++ src/pages/Home/components/CoffeeList/styles.ts
@@ -8,3 +8,52 @@ export const TitleBox = styled.div`
     color: ${(props) => props.theme['gray-800']};
   }
 `
+
+export const CoffeeListBox = styled.div`
+  padding: 4rem 0;
+`
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+
+  border-top-right-radius: 30px;
+  border-bottom-left-radius: 30px;
+
+  background-color: ${(props) => props.theme['gray-200']};
+  width: 16rem;
+  height: 19.375rem;
+
+  img {
+    width: 120px;
+    height: 120px;
+
+    margin-top: -6rem;
+  }
+
+  span {
+    background-color: ${(props) => props.theme['yellow-100']};
+    color: ${(props) => props.theme['yellow-700']};
+    border-radius: 10px;
+    padding: 0.2rem 1rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 0.625rem;
+    margin: 0.5rem 0;
+  }
+
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 800;
+    font-size: 1.25rem;
+    color: ${(props) => props.theme['gray-800']};
+  }
+  p {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['gray-600']};
+    padding: 0.5rem 1rem;
+  }
+`
