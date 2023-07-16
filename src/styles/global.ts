b15ed91ebb78c570455838fcde17c451b9a434de
Zhydani <zhydani@gmail.com>
2023-07-16 10:23:09 -0300

    fix!: corrigindo styled components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,12 +9,11 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: none;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body {
-    background: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']};
+    background: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme['gray-700']};
     -webkit-font-smoothing: antialiased;
   }
 
